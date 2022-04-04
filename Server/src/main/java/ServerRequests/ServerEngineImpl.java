ea7decdf05631d42859d3b288b40369a447c0e7e
DEHNC121 <D.demiankov@student.uj.edu.pl>
2022-04-04 12:16:02 +0200

    test

---
+++ Server/src/main/java/ServerRequests/ServerEngineImpl.java
@@ -0,0 +1,16 @@
+package ServerRequests;
+
+import java.util.List;
+
+public class ServerEngineImpl implements ServerEngine{
+    @Override
+    public void add(ServerQuestion question, ServerAnswer a) {
+
+    }
+
+    @Override
+    public List<ServerAnswer> query(ServerQuestion question) {
+
+        return null;
+    }
+}
