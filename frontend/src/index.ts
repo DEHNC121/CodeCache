1b6c46f3007d80c9da418fa0dd21bb598235e334
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2022-04-29 20:34:11 +0200

    Change display

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,7 +1,7 @@
 import {get_answers, html_inject, insert_answers} from "./rest";
 
 // inject html
-const parent_name = "appbar";
+const parent_name = "rso";
 document.getElementById(parent_name).insertAdjacentHTML("afterbegin", html_inject());
 
 // get query parameters
@@ -9,7 +9,7 @@ const url = new URL(document.URL);
 let query_params = url.searchParams.get('q');
 console.log(query_params);
 
-// REST API communication
+//REST API communication
 get_answers(query_params).then(response => {
     console.log(response);
     insert_answers(response, document.getElementById("CodeCache-background"));
